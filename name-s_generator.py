147986eb0824822dea46cc76d1f37b85d3d51edd
Anonymous <89711616+Troy-Mccarter@users.noreply.github.com>
2021-10-11 05:55:35 +0530

    Create name-s_generator.py

---
+++ name-s_generator.py
@@ -0,0 +1,13 @@
+import names  # pip install names
+
+# used to get the first name
+first_name = names.get_first_name()   
+print(first_name)
+
+# used to get the last name
+last_name = names.get_last_name()  
+print(last_name)
+
+# used to get the full name
+full_name = names.get_full_name()  
+print(full_name)
